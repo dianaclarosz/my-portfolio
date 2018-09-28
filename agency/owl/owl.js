6df11a763d9742135a220bd50c772e265ba4e683
dianaclarosz <dianaclarosza@gmail.com>
2018-09-28 15:10:41 -0400

    this is my one page portfolio website!

---
+++ agency/owl/owl.js
@@ -0,0 +1,21 @@
+$('#customers').owlCarousel({
+    loop:true,
+    margin:10,
+    smartSpeed:4000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+//owl carousel
+$(".owl-prev").html('<i class="fa fa-chevron-left fa-2x"></i>');
+$(".owl-next").html('<i class="fa fa-chevron-right fa-2x"></i>');
